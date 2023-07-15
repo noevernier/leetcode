7d48613e2365d3ddd3d5f46c7f4b607b0e2f8f60
Noé Vernier <vernier.noe@gmail.com>
2023-07-15 19:00:21 +0200

    Solve 167

---
+++ solutions/167_two_sum_II/two_sum_II.py
@@ -0,0 +1,11 @@
+from ast import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers)-1
+        while numbers[l] + numbers[r] != target:
+            if numbers[l] + numbers[r] < target:
+                l+=1
+            else:
+                r-=1
+        return [l, r]
