1ddb8c852c70bc26660ef05f272116baef678e25
Noé Vernier <noe.vernier@telecom-paris.fr>
2024-01-06 21:43:01 +0100

    Add solution for detecting cycle in a linked list

---
+++ solutions/141_linked_list_cycle/linked_list_cycle.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+# Solution optimal in space
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = []
+        while head:
+            if head in visited:
+                return True
+            visited.append(head)
+            head = head.next
+        return False
