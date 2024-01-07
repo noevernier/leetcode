34c0181e8e6af099565df1ab5f98992822b6d781
Noé Vernier <noe.vernier@telecom-paris.fr>
2024-01-07 20:41:01 +0100

    Refactor linked list cycle detection algorithm

--- solutions/141_linked_list_cycle/linked_list_cycle.py
+++ solutions/141_linked_list_cycle/linked_list_cycle.py
@@ -17,3 +17,17 @@ class Solution:
             visited.append(head)
             head = head.next
         return False
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while slow and fast:
+            slow = slow.next
+            if not fast.next:
+                return False
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
